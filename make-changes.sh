9c13681d8b479fac170e8efbd20b3167cedb62da
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2017-02-16 13:33:14 +0100

    make-changes.sh: use relative paths
    Use relative paths for the “from” argument to `cp`.
    
    This makes the script more robust in case I choose to move the
    repository around (the root directory).  I don't see any benefit of
    using absolute paths in this case.

--- make-changes.sh
+++ make-changes.sh
@@ -4,8 +4,8 @@
 # to deal with what are effectively "stale caches" of the the keymaps.
 sudo trash-put /var/lib/xkb/*
 
-sudo cp ~/xkb/no_p1 /usr/share/X11/xkb/symbols/no_p1
-sudo cp ~/xkb/no_1 /usr/share/X11/xkb/symbols/no_1
-sudo cp ~/xkb/latin_p1 /usr/share/X11/xkb/symbols/latin_p1
+sudo cp no_p1 /usr/share/X11/xkb/symbols/no_p1
+sudo cp no_1 /usr/share/X11/xkb/symbols/no_1
+sudo cp latin_p1 /usr/share/X11/xkb/symbols/latin_p1
 
 sudo restart lightdm
