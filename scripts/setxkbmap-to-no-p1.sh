8cef20da5a3b621c46424cd2b041eacc7f928669
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2017-08-31 00:23:50 +0200

    Remove outdated scripts
    I don’t need any of these scripts any more.

--- scripts/setxkbmap-to-no-p1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Set keyboard mapping back to my usual setup.
-setxkbmap "no_p1(nodeadkeys)"
--- scripts/wrapper-setxkbmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Set keyboard to the argument, where the argument is one of the
-# mappings in the file.
-# NOTE: the `-compat` option is so that level 2 latch works properly.
-setxkbmap "twin-dexter($1)" -compat my-complete -print | xkbcomp -I"xkb-tree" - "$DISPLAY"
