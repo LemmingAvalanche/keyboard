765e2e4406b47abb8add01216ce33f7990325a26
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2017-10-30 21:24:07 +0100

    Add Nix expression for the project

---
+++ default.nix
@@ -0,0 +1,16 @@
+# Run `nix-shell --pure` in the current directory to get access to the
+# packages that you need to run `scripts/keyboard-setup`.
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+in rec {
+  environment = stdenv.mkDerivation rec {
+    name = "keyboard-environment";
+    version = "1";
+    src = ./.;
+    buildInputs = [
+      pkgs.xorg.xkbcomp
+      pkgs.xorg.setxkbmap
+    ];
+  };
+}
