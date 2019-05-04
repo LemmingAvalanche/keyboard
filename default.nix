cf4181aa49520c803123d8e1bd9ee893a75fdc3d
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2019-05-04 23:38:10 +0200

    Remove xkbcomp(1) override
    xkbcomp 1.4.2 seems to work fine with the current version of
    `twin-dexter`.

--- default.nix
+++ default.nix
@@ -3,14 +3,6 @@
 let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
-  # Override `src` in order to fetch the source for Xbkcomp 1.3.1 since
-  # Xkbcomp does not work as I want.
-  xkbcomp = pkgs.xorg.xkbcomp.overrideAttrs (oldAttrs: rec {
-     src = pkgs.fetchurl {
-        url = "http://xorg.freedesktop.org/archive/individual/app/xkbcomp-1.3.1.tar.bz2";
-        sha256 = "0gcjy70ppmcl610z8gxc7sydsx93f8cm8pggm4qhihaa1ngdq103";
-     };
-  });
 in rec {
   environment = stdenv.mkDerivation rec {
     name = "keyboard-environment";
@@ -19,7 +11,7 @@ in rec {
     buildInputs = [
       pkgs.xorg.xkeyboardconfig
       pkgs.xorg.setxkbmap
-      xkbcomp
+      pkgs.xorg.xkbcomp
     ];
   };
 }
